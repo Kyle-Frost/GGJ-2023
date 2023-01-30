6d98f31ced5d2eb1be8316692819893e00af1864
Kyle Frost <ikjb@live.co.uk>
2023-01-30 19:12:29 +0000

    Added SO for recipes and items

---
+++ Game Jam 2023/Assets/Recipe.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Recipe", menuName = "Recipe")]
+public class Recipe : ScriptableObject
+{
+    public Item itemOne;
+    public Item itemTwo;
+
+    public Item createdItem;
+}
